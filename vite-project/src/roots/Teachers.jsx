cc01cb47fc2eb52c8b4af730ae7f54980c29c6dc
Jennifer Zhuang <118019266+jzhuang03@users.noreply.github.com>
2024-05-24 10:19:08 -0400

    new branch created

--- vite-project/src/roots/Teachers.jsx
+++ vite-project/src/roots/Teachers.jsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { collection, getDocs, doc, updateDoc, addDoc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  doc,
+  updateDoc,
+  addDoc,
+} from "firebase/firestore";
 import { db } from "../../firebase";
 import "../styles/Teachers.css";
 import {
@@ -9,13 +15,21 @@ import {
   Typography,
   Button,
   Grid,
-  RadioGroup, Radio, Select, MenuItem, FormControl, TextField, FormLabel, FormControlLabel, Checkbox
+  RadioGroup,
+  Radio,
+  Select,
+  MenuItem,
+  FormControl,
+  TextField,
+  FormLabel,
+  FormControlLabel,
+  Checkbox,
 } from "@mui/material";
 
 const Teachers = () => {
   const [allTeachers, setAllTeachers] = useState([]);
   const [teachers, setTeachers] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [selectedTeacher, setSelectedTeacher] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
   const [editedTeacher, setEditedTeacher] = useState({});
@@ -26,11 +40,10 @@ const Teachers = () => {
   const [Subject, setSubject] = useState("");
   const [Age, setAge] = useState("");
 
-
   const fetchTeachers = async () => {
     try {
-      const querySnapshot = await getDocs(collection(db, 'teachers'));
-      const teachersList = querySnapshot.docs.map(doc => ({
+      const querySnapshot = await getDocs(collection(db, "teachers"));
+      const teachersList = querySnapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
       }));
@@ -49,12 +62,13 @@ const Teachers = () => {
     const searchValue = e.target.value;
     setSearchTerm(searchValue);
 
-    if (searchValue === '') {
+    if (searchValue === "") {
       setTeachers(allTeachers);
     } else {
-      const filteredTeachers = allTeachers.filter(teacher =>
-        teacher.First.toLowerCase().includes(searchValue.toLowerCase()) ||
-        teacher.Last.toLowerCase().includes(searchValue.toLowerCase())
+      const filteredTeachers = allTeachers.filter(
+        (teacher) =>
+          teacher.First.toLowerCase().includes(searchValue.toLowerCase()) ||
+          teacher.Last.toLowerCase().includes(searchValue.toLowerCase())
       );
       setTeachers(filteredTeachers);
     }
@@ -77,12 +91,12 @@ const Teachers = () => {
 
   const handleSaveClick = async () => {
     try {
-      const teacherRef = doc(db, 'teachers', selectedTeacher.id);
+      const teacherRef = doc(db, "teachers", selectedTeacher.id);
       await updateDoc(teacherRef, editedTeacher);
       setSelectedTeacher(editedTeacher);
       setIsEditing(false);
 
-      const updatedTeachers = teachers.map(teacher =>
+      const updatedTeachers = teachers.map((teacher) =>
         teacher.id === editedTeacher.id ? editedTeacher : teacher
       );
       setTeachers(updatedTeachers);
@@ -97,8 +111,6 @@ const Teachers = () => {
     setIsAddingTeacher((prevState) => !prevState);
   };
 
-
-
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -111,29 +123,25 @@ const Teachers = () => {
       First,
       Last,
       Subject,
-      Age, 
+      Age,
     };
 
     try {
       const docRef = await addDoc(collection(db, "teachers"), newTeacher);
       //console.log("Document written with ID: ", docRef.id);
-      fetchTeachers(); 
-      
+      fetchTeachers();
 
       // Clear form fields after submission for better user experience
       setFirst("");
       setLast("");
-      setSubject(""); 
-      setAge(""); 
-
+      setSubject("");
+      setAge("");
     } catch (error) {
       console.error("Error adding document: ", error);
     }
 
-
-
-    return; 
-  }
+    return;
+  };
 
   return (
     <div>
@@ -146,61 +154,65 @@ const Teachers = () => {
         <div className="overlay"></div>
         <h1 className="teacherScreenHeader">Teacher Directory</h1>
       </div>
-      <hr className = "homePageHr"></hr>
-      <Card className="main-wrapper" sx={{boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.2)'}}>
+      <hr className="homePageHr"></hr>
+      <Card
+        className="main-wrapper"
+        sx={{ boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.2)" }}
+      >
         <div className="left-panel">
           <h1>All Teachers</h1>
           <p>Browse through the list of all teachers</p>
 
+          <div className="addTeacher-option" style={{ marginBottom: "40px" }}>
+            <figcaption onClick={handleAddOption}>
+              Can't find your teacher? {isAddingTeacher ? "▲" : "▼"}
+            </figcaption>
+            <br />
+            {isAddingTeacher && (
+              <form onSubmit={handleSubmit}>
+                <TextField
+                  label="First Name"
+                  value={First}
+                  onChange={(e) => setFirst(e.target.value)}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  label="Last Name"
+                  value={Last}
+                  onChange={(e) => setLast(e.target.value)}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  label="Subject"
+                  value={Subject}
+                  onChange={(e) => setSubject(e.target.value)}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  label="Age"
+                  value={Age}
+                  onChange={(e) => setAge(e.target.value)}
+                  fullWidth
+                  margin="normal"
+                />
 
-
-          <div className="addTeacher-option" style={{ marginBottom: '40px' }}>
-      <figcaption onClick={handleAddOption}>
-        Can't find your teacher? {isAddingTeacher ? "▲" : "▼"}
-      </figcaption>
-      <br />
-      {isAddingTeacher && (
-        <form onSubmit={handleSubmit}>
-          <TextField
-            label="First Name"
-            value={First}
-            onChange={(e) => setFirst(e.target.value)}
-            fullWidth
-            margin="normal"
-          />
-          <TextField
-            label="Last Name"
-            value={Last}
-            onChange={(e) => setLast(e.target.value)}
-            fullWidth
-            margin="normal"
-          />
-          <TextField
-            label="Subject"
-            value={Subject}
-            onChange={(e) => setSubject(e.target.value)}
-            fullWidth
-            margin="normal"
-          />
-            <TextField
-            label="Age"
-            value={Age}
-            onChange={(e) => setAge(e.target.value)}
-            fullWidth
-            margin="normal"
-          />
-          <Button
-            sx={{ background: '#147a7c', '&:hover': { backgroundColor: '#0f5f60' } }}
-            type="submit"
-            variant="contained"
-            color="primary"
-          >
-            Add Teacher
-          </Button>
-        </form>
-      )}
-    </div>
-
+                <Button
+                  sx={{
+                    background: "#147a7c",
+                    "&:hover": { backgroundColor: "#0f5f60" },
+                  }}
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                >
+                  Add Teacher
+                </Button>
+              </form>
+            )}
+          </div>
 
           <div className="search">
             <input
@@ -213,10 +225,20 @@ const Teachers = () => {
 
           <div className="teacher-list">
             {teachers.map((teacher) => (
-              <div className="teacher-item" key={teacher.id} onClick={() => handleTeacherClick(teacher)}>
-                <img src={teacher.photoURL || "/defaultAvatar.png"} alt={`${teacher.First} ${teacher.Last}`} className="teacher-avatar" />
+              <div
+                className="teacher-item"
+                key={teacher.id}
+                onClick={() => handleTeacherClick(teacher)}
+              >
+                <img
+                  src={teacher.photoURL || "/defaultAvatar.png"}
+                  alt={`${teacher.First} ${teacher.Last}`}
+                  className="teacher-avatar"
+                />
                 <div className="teacher-info">
-                  <p className="teacher-name">{teacher.First} {teacher.Last}</p>
+                  <p className="teacher-name">
+                    {teacher.First} {teacher.Last}
+                  </p>
                   <p className="teacher-department">{teacher.Subject}</p>
                   <p className="teacher-phone">Age: {teacher.Age}</p>
                 </div>
@@ -228,24 +250,47 @@ const Teachers = () => {
         <div className="right-panel">
           {selectedTeacher ? (
             <div className="teacher-detail">
-              <img src={selectedTeacher.photoURL || "/defaultAvatar.png"} alt={`${selectedTeacher.First} ${selectedTeacher.Last}`} className="teacher-avatar-large" />
-              <h3>{selectedTeacher.First} {selectedTeacher.Last}</h3>
+              <img
+                src={selectedTeacher.photoURL || "/defaultAvatar.png"}
+                alt={`${selectedTeacher.First} ${selectedTeacher.Last}`}
+                className="teacher-avatar-large"
+              />
+              <h3>
+                {selectedTeacher.First} {selectedTeacher.Last}
+              </h3>
               <div className="teacher-contact">
                 <h4>Contact Information</h4>
                 {isEditing ? (
                   <div>
                     <label>Office Tel: </label>
-                    <input type="text" name="OfficeTel" value={editedTeacher.OfficeTel || ''} onChange={handleInputChange} /><br/>
+                    <input
+                      type="text"
+                      name="OfficeTel"
+                      value={editedTeacher.OfficeTel || ""}
+                      onChange={handleInputChange}
+                    />
+                    <br />
                     <label>Mobile: </label>
-                    <input type="text" name="Mobile" value={editedTeacher.Mobile || ''} onChange={handleInputChange} /><br/>
+                    <input
+                      type="text"
+                      name="Mobile"
+                      value={editedTeacher.Mobile || ""}
+                      onChange={handleInputChange}
+                    />
+                    <br />
                     <label>Email: </label>
-                    <input type="text" name="Email" value={editedTeacher.Email || ''} onChange={handleInputChange} />
+                    <input
+                      type="text"
+                      name="Email"
+                      value={editedTeacher.Email || ""}
+                      onChange={handleInputChange}
+                    />
                   </div>
                 ) : (
                   <div>
-                    <p>Office Tel: {selectedTeacher.OfficeTel || 'N/A'}</p>
-                    <p>Mobile: {selectedTeacher.Mobile || 'N/A'}</p>
-                    <p>Email: {selectedTeacher.Email || 'N/A'}</p>
+                    <p>Office Tel: {selectedTeacher.OfficeTel || "N/A"}</p>
+                    <p>Mobile: {selectedTeacher.Mobile || "N/A"}</p>
+                    <p>Email: {selectedTeacher.Email || "N/A"}</p>
                   </div>
                 )}
               </div>
@@ -254,14 +299,25 @@ const Teachers = () => {
                 {isEditing ? (
                   <div>
                     <label>Department: </label>
-                    <input type="text" name="Subject" value={editedTeacher.Subject} onChange={handleInputChange} /><br/>
+                    <input
+                      type="text"
+                      name="Subject"
+                      value={editedTeacher.Subject}
+                      onChange={handleInputChange}
+                    />
+                    <br />
                     <label>Supervisor: </label>
-                    <input type="text" name="Supervisor" value={editedTeacher.Supervisor || ''} onChange={handleInputChange} />
+                    <input
+                      type="text"
+                      name="Supervisor"
+                      value={editedTeacher.Supervisor || ""}
+                      onChange={handleInputChange}
+                    />
                   </div>
                 ) : (
                   <div>
                     <p>Department: {selectedTeacher.Subject}</p>
-                    <p>Supervisor: {selectedTeacher.Supervisor || 'N/A'}</p>
+                    <p>Supervisor: {selectedTeacher.Supervisor || "N/A"}</p>
                   </div>
                 )}
               </div>
@@ -270,24 +326,59 @@ const Teachers = () => {
                 {isEditing ? (
                   <div>
                     <label>Pronouns: </label>
-                    <input type="text" name="Pronouns" value={editedTeacher.Pronouns || ''} onChange={handleInputChange} /><br/>
+                    <input
+                      type="text"
+                      name="Pronouns"
+                      value={editedTeacher.Pronouns || ""}
+                      onChange={handleInputChange}
+                    />
+                    <br />
                     <label>Birthday: </label>
-                    <input type="text" name="Birthday" value={editedTeacher.Birthday || ''} onChange={handleInputChange} /><br/>
+                    <input
+                      type="text"
+                      name="Birthday"
+                      value={editedTeacher.Birthday || ""}
+                      onChange={handleInputChange}
+                    />
+                    <br />
                     <label>City: </label>
-                    <input type="text" name="City" value={editedTeacher.City || ''} onChange={handleInputChange} />
+                    <input
+                      type="text"
+                      name="City"
+                      value={editedTeacher.City || ""}
+                      onChange={handleInputChange}
+                    />
                   </div>
                 ) : (
                   <div>
-                    <p>Pronouns: {selectedTeacher.Pronouns || 'N/A'}</p>
-                    <p>Birthday: {selectedTeacher.Birthday || 'N/A'}</p>
-                    <p>City: {selectedTeacher.City || 'N/A'}</p>
+                    <p>Pronouns: {selectedTeacher.Pronouns || "N/A"}</p>
+                    <p>Birthday: {selectedTeacher.Birthday || "N/A"}</p>
+                    <p>City: {selectedTeacher.City || "N/A"}</p>
                   </div>
                 )}
               </div>
               {isEditing ? (
-                <Button sx={{background: '#147a7c', '&:hover': {backgroundColor: '#0f5f60',},}}onClick={handleSaveClick} variant="contained">Save</Button>
+                <Button
+                  sx={{
+                    background: "#147a7c",
+                    "&:hover": { backgroundColor: "#0f5f60" },
+                  }}
+                  onClick={handleSaveClick}
+                  variant="contained"
+                >
+                  Save
+                </Button>
               ) : (
-                <Button sx={{background: '#147a7c', '&:hover': {backgroundColor: '#0f5f60',},}} onClick={handleEditClick} variant="contained">Edit</Button>
+                <Button
+                  sx={{
+                    background: "#147a7c",
+                    "&:hover": { backgroundColor: "#0f5f60" },
+                  }}
+                  onClick={handleEditClick}
+                  variant="contained"
+                >
+                  Edit
+                </Button>
               )}
             </div>
           ) : (
